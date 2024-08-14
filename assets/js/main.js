e78f0449974b4cb226f8b54964a2cef75fceae8d
cassiaqueiroz <cassiarsqueiroz@gmail.com>
2024-08-14 16:54:21 -0400

    editando twitter

--- assets/js/main.js
+++ assets/js/main.js
@@ -312,12 +312,3 @@
    */
   new PureCounter();
 })();
-
-twttr.widgets.createTimeline(
-  {
-    sourceType: "list",
-    ownerScreenName: "TwitterDev",
-    slug: "national-parks"
-  },
-  document.getElementById("container")
-);
